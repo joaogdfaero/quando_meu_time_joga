d66cdd40ce52d26028cf2642acc967efec571c33
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2024-02-14 10:31:57 -0300

    removed comments

--- myproject/nextmatchapp/views.py
+++ myproject/nextmatchapp/views.py
@@ -5,10 +5,8 @@ from django.http import JsonResponse
 def get_next_games(request, time_id):
     url = f"https://api.api-futebol.com.br/v1/times/{time_id}/partidas/proximas"
     
-    # Retrieve API key from environment variable
     api_key = os.getenv('API_KEY')
     
-    # Add API key to the Authorization header
     headers = {
         'Authorization': f'Bearer {api_key}'
     }
